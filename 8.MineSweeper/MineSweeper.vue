4e64be7a773813a47de3cf6e24faa56547ae8bd0
cksdnr3 <cksdnr3@naver.com>
2021-03-09 23:33:56 +0900

    21 / 03 / 09 MineSweeper with VanillaJS

---
+++ 8.MineSweeper/MineSweeper.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <mine-form></mine-form>
+    <div>{{ timer }}</div>
+    <table-component></table-component>
+    <div>{{ result }}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import store from "./store";
+import MineForm from './MineForm'
+
+export default {
+  components: {
+    MineForm
+  },
+  computed: {
+    ...mapState(['timer', 'result'])
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style>
+table {
+  border-collapse: collapse;
+}
+td {
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+}
+</style>
---
+++ min_sweeper_JS/MineSweeper.js
@@ -0,0 +1,67 @@
+var tbody = document.querySelector('#table tbody');
+var dataSet = [];
+
+document.querySelector('#exec').addEventListener('click', function() {
+    tbody.innerHTML = '';
+    console.log("dataSet: " + dataSet)
+    dataSet = [];
+    var hor = parseInt(document.querySelector('#hor').value);
+    var ver = parseInt(document.querySelector('#ver').value);
+    var mine = parseInt(document.querySelector('#mine').value);
+    console.log(hor, ver, mine);
+
+    var candidates = Array(hor * ver)
+        .fill(undefined)
+        .map((item, index) => {
+        return index;
+    });
+
+    var shuffle = [];
+    while (candidates.length > hor * ver - mine) {
+        var picked = candidates.splice(Math.floor(Math.random() * candidates.length), 1)[0]
+        shuffle.push(picked);
+    }
+
+    for (var i = 0; i < ver; i++) {
+        var arr = [];
+        dataSet.push(arr);
+        var tr = document.createElement('tr');
+        for (var j = 0; j < hor; j++) {
+            arr.push(1);
+            var td = document.createElement('td');
+            td.addEventListener('contextmenu', function (e) {
+                e.preventDefault();
+                // target은 이벤트(contextmenu)가 발생한 태그를 의미한다.
+                // currentTarget은 eventListener가 달린 태그를 의미한다.
+                var parentTr = e.currentTarget.parentNode;
+                var parentTbody = e.currentTarget.parentNode.parentNode;
+                var rowIdx = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+                var colIdx = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
+
+                if (e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X') {
+                    e.currentTarget.textContent = '!';
+
+                } else if (e.currentTarget.textContent === '!') {
+                    e.currentTarget.textContent = '?';
+
+                } else if (e.currentTarget.textContent === '?') {
+                    if (dataSet[rowIdx][colIdx] === 1) {
+                        e.currentTarget.textContent = '';
+                    } else if (dataSet[rowIdx][colIdx] === 'X') {
+                        e.currentTarget.textContent = 'X';
+                    }
+                }
+
+            });
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    for (var k = 0; k < shuffle.length; k++) {
+        var row = Math.floor(shuffle[k] / 10);
+        var col = shuffle[k] % 10;
+        console.log(row, col);
+        tbody.children[row].children[col].textContent = 'X';
+        dataSet[row][col] = 'X';
+    }
+});
